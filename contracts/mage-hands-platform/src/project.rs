db0e7d1ca7dfd94624a423f349fa0f8fb96a2fc1
Ben Adams <darwinzero@gmail.com>
2022-10-26 22:54:53 +1300

    update snip24 reward init

--- contracts/mage-hands-platform/src/project.rs
+++ contracts/mage-hands-platform/src/project.rs
@@ -46,7 +46,7 @@ pub struct RewardMessage {
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, Debug,)]
 pub struct Snip24RewardInit {
-    pub reward_snip24_code_id: u32,
+    pub reward_snip24_code_id: u64,
     pub reward_snip24_code_hash: String,
 
     // snip24 params
@@ -60,28 +60,19 @@ pub struct Snip24RewardInit {
     pub enable_mint: bool,
     pub enable_burn: bool,
 
-    // total number of tokens to hold in vesting contract
-    pub amount: Uint128,
-
     // timeline of release of tokens to contributors
-    // sum of per_mille in each VestingEvent must sum to 1000
-    pub contributors_vesting_schedule: Vec<VestingEvent>,
-    // permille of tokens that are reserved for contributors
-    pub contributors_per_mille: u16,
+    pub contributor_vesting_schedule: Vec<VestingEvent>,
     // minimum contribution to be eligible for reward
-    pub minimum_contribution: Uint128,
+    pub minimum_contribution: Option<Uint128>,
     // maximum amount of contribution applied to reward
     // if maximum == 0 or < minimum, then there is no maximum
-    pub maximum_contribution: Uint128,
+    pub maximum_contribution: Option<Uint128>,
     // contribution weighting: one of linear, sqrt, or log
     // weighting is applied after minimum and maximum is applied
     pub contribution_weight: u8,
 
     // timeline of release of tokens to creator
-    // sum of per_mille in each VestingEvent must sum to 1000
     pub creator_vesting_schedule: Vec<VestingEvent>,
-    // permille of tokens that are reserved for creator
-    pub creator_per_mille: u16,
     // addresses to evenly distribute coins to
     // if none, will distribute to project creator address
     pub creator_addresses: Option<Vec<Addr>>,
@@ -91,7 +82,7 @@ pub struct Snip24RewardInit {
 #[derive(Serialize, Deserialize, Clone, PartialEq, Debug,)]
 pub struct VestingEvent {
     pub block: u64,
-    pub per_mille: u16,
+    pub amount: Uint128,
 }
 
 impl InitCallback for ProjectInstantiateMsg {
